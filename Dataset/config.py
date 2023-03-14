93ee3f21316e6184882aa5d87d5e79ac7fbd5f5b
P <30462819+ptrpfa@users.noreply.github.com>
2023-03-14 16:52:46 +0800

    added crawling of webpages

---
+++ Dataset/config.py
@@ -0,0 +1,22 @@
+# Program Flags
+CRAWL_API = False   # Boolean to configure the crawling of data from PAJ's API
+CRAWL_WEB = True    # Boolean to configure the crawling of data from the bus webpages
+
+# PAJ API
+api_endpoint = 'https://dataapi.paj.com.my/api/v1'
+api_key = 'a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a'
+
+# Bus Links
+schedule_link = "https://businterchange.net/johorbus/routes/routeinfo.php?service="
+loop_buses = ["P101", "P106", "P202", "P403"]       # Buses that run in a loop
+one_way_buses = ["P102", "P211", "P411"]     # Buses that only run one way
+
+# Files and folders
+data_folder = "json/"
+file_routes = data_folder + "routes.json"
+file_geo_routes = data_folder + "geo_routes.json"
+file_route_schedule = data_folder + "routes_schedule.json"
+file_operators = data_folder + "operators.json"
+file_bus_live = data_folder + "bus_live.json"
+file_bus_stop = data_folder + "bus_stops.json"
+file_bus_list = data_folder + "bus_list.json"
--- Dataset/crawl.py
+++ Dataset/crawl.py
@@ -1,21 +1,8 @@
 import requests
 import json
-
-# Configurations
-CRAWL = False # Boolean to configure the crawling of data from PAJ's API
-api_endpoint = 'https://dataapi.paj.com.my/api/v1'
-api_key = 'a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a'
-headers = {'api-key': api_key}
-
-# Files
-data_folder = "json/"
-file_routes = data_folder + "routes.json"
-file_geo_routes = data_folder + "geo_routes.json"
-file_route_schedule = data_folder + "routes_schedule.json"
-file_operators = data_folder + "operators.json"
-file_bus_live = data_folder + "bus_live.json"
-file_bus_stop = data_folder + "bus_stops.json"
-file_bus_list = data_folder + "bus_list.json"
+import re
+from config import *
+from bs4 import BeautifulSoup as bs_4
 
 # Function to write to a file
 def save_file(text, file_name):
@@ -24,6 +11,9 @@ def save_file(text, file_name):
 
 # Function to crawl PAJ's API (limited to 6 API calls per minute)
 def crawl_api():
+    # Set HTTP header
+    headers = {'api-key': api_key}
+
     # Get bus route data
     routes = requests.get(api_endpoint + '/route', headers=headers, verify=False).text
     save_file(routes, file_routes)
@@ -52,8 +42,52 @@ def crawl_api():
     bus_list = requests.get(api_endpoint + '/bus-list', headers=headers, verify=False).text
     save_file(bus_list, file_bus_list)
 
+# Function to crawl webpages for bus schedule information
+def crawl_web():
+    # Initialise bus schedule
+    bus_schedule = {}
+
+    # Loop through each bus (loop)
+    for bus in loop_buses:
+        list_timings = []
+        response = requests.get(schedule_link + bus)
+        response = bs_4(response.text, "lxml")
+        response = response.find_all('table', attrs={'border':'1'})[1] # Get table
+        response = response.find_all('div') # Get rows
+        # Loop through each row to get the bus timings
+        for row in response:
+            list_timings.append(re.search("^<div.*>(\d*)<\/div>$", str(row)).group(1))
+        bus_schedule[bus] = list_timings
+
+    # Loop through each one way bus
+    for bus in one_way_buses:
+        list_timings = []
+        response = requests.get(schedule_link + bus)
+        response = bs_4(response.text, "lxml")
+        response = response.find_all('table', attrs={'border':'1'})[1] # Get table
+        
+        # Get first direction
+        first = response.find_all('tr')[4]
+        first = first.find_all('div') # Get rows
+        # Loop through each row to get the bus timings
+        for row in first:
+            list_timings.append(re.search("^<div.*>(\d*)<\/div>$", str(row)).group(1))
+        bus_schedule[bus + '-01'] = list_timings
+        
+        # Get second direction
+        list_timings = []
+        second = response.find_all('tr')[7]
+        second = second.find_all('div') # Get rows
+        # Loop through each row to get the bus timings
+        for row in second:
+            list_timings.append(re.search("^<div.*>(\d*)<\/div>$", str(row)).group(1))
+        bus_schedule[bus + '-02'] = list_timings
+
+    # Return bus schedule obtained
+    return bus_schedule
+
 # Program Entrypoint
-if(CRAWL):
+if(CRAWL_API):
     crawl_api()
 
 """ Initialise API data """
@@ -75,3 +109,7 @@ geo_routes = geo_routes['data']
 # bus_list = bus_list['data']
 # operators = json.load(open(file_operators))             # Bus Operators (Unused)
 # operators = operators['data']
+
+if(CRAWL_WEB):
+    bus_schedule = crawl_web()
+    print(bus_schedule)
