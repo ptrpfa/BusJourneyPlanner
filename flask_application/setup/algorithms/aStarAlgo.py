75fccf458f1ebd080413ec3a0b290c47e761f0af
dinieaziz <nurdinieaziz@gmail.com>
2023-03-25 13:50:33 +0800

    optimised a* with PQ

--- flask_application/setup/algorithms/aStarAlgo.py
+++ flask_application/setup/algorithms/aStarAlgo.py
@@ -4,6 +4,8 @@ import pickle
 import networkx as nx
 import matplotlib.pyplot as plt
 
+from queue import PriorityQueue
+
 BUSSPEED = 70 #km/h  
 
 connection = mysql.connector.connect(
@@ -13,41 +15,13 @@ connection = mysql.connector.connect(
     database = "DSA_JP"
 )
 
-# Generate graph to view weight and edges
-def VisualiseGraph(multi_graph):
-    # Create a layout for the nodes
-    pos = nx.spring_layout(multi_graph)
-
-    # Draw nodes and edges with labels
-    nx.draw(multi_graph, pos, with_labels=True)
-
-    # Get a dictionary of edge labels
-    edge_labels = {}
-    for (u, v, key, data) in multi_graph.edges(keys=True, data=True):
-        route = data['route']
-        weight = data['weight']
-        label = (route, weight)
-        if (u, v) in edge_labels:
-            edge_labels[(u, v)].append(label)
-        else:
-            edge_labels[(u, v)] = [label]
-
-    # Draw edge labels
-    for edge, labels in edge_labels.items():
-        label = '\n'.join([f"{label[0]}: {label[1]}" for label in labels])
-        nx.draw_networkx_edge_labels(multi_graph, pos, edge_labels={(edge[0], edge[1]): label})
-
-
-    # Display the visualization and save to file
-    # plt.savefig("amogus.jpg", format='jpg')
-    plt.show()
-
 # Djikstra but with heuristic function
 def aStarAlgo(startNode, endNode):
-    # unvisited is list of nodes which has been visited but neighbors havent all been inspected
-    unvisited = set([startNode])
+    # Unvisited is priority queue of nodes which has been visited but neighbors havent all been inspected
+    unvisited = PriorityQueue()
+    unvisited.put((0, startNode))  # Priority is the shortestTime() + heuristicTime(), which is 0 for startNode
     
-    # visited is list of nodes which has been visited and neighbors have been inspected
+    # Set of nodes which has been visited and neighbors have been inspected
     visited = set([])
 
     # Store time from startNode to other nodes, default value is infinity
@@ -59,68 +33,50 @@ def aStarAlgo(startNode, endNode):
     previous[startNode] = startNode
 
     # Traverse through all nodes that hasnt been visited
-    while len(unvisited) > 0:
+    while not unvisited.empty():
         # Checking the neighbors of the current node
-        neighbour = None
+        f, neighbour = unvisited.get()  # get the node with smallest f(n) value
 
-        # Finding node with the lowest value of estimatedTotalTime(): shortestTime() + heuristicTime(), f(n) = g(n) + h(n)
-        for count1 in unvisited:
-            if neighbour == None or time[count1] + getHeuristic(count1, endNode) < time[neighbour] + getHeuristic(neighbour, endNode):
-                neighbour = count1
+        # If node is endNode, goal reached and reverse path to show travel sequence
+        if neighbour == endNode:
+            tempPath = {}
+            totalTime = time[neighbour]
 
-        # If no more neighbours and is not endNode, no path found
-        if neighbour == None:
-            print("No path found")
-            return None
+            # Take note of current and previous node to get route info and reverse the list
+            while previous[neighbour] != neighbour:
+                u = previous[neighbour]
+                data = graph.get_edge_data(u, neighbour)
+                tempPath[str(u) + "-" + str(neighbour)] = data[0]['bus']
+                neighbour = u
+
+            # Reverse the dictionary and print sequential steps
+            reversedDict = {}
+            count = 1
+            print("No | Stops        (BusID)")
+            print("--------------------------")
+            
+            for key, value in reversed(tempPath.items()):
+                reversedDict[key] = "BusID - " + str(value)
+                print(f"{count:<3}| {key:<12}({reversedDict[key]})")
+                count += 1
+
+            # Prints total duration of journey
+            print("\nJourney time in minutes:", totalTime)
+            return tempPath
+
+        # Add neighbour to visited because edges will be inspected
+        visited.add(neighbour)
 
         for (node, weight) in getNeighbours(neighbour):
-            # If node is endNode, goal reached and reverse path to show travel seqeuence
-            if neighbour == endNode:
-                tempPath = {}
-                totalTime = time[neighbour]
-
-                # Take note of current and previous node to get route info and reverse the list
-                while previous[neighbour] != neighbour:
-                    u = previous[neighbour]
-                    data = graph.get_edge_data(u, neighbour)
-                    tempPath[str(u) + "-" + str(neighbour)] = data[0]['bus']
-                    neighbour = u
-
-                # Reverse the dictionary and template to display format
-                reversedDict = {}
-                count = 1
-                print("No | Stops        (BusID)")
-                print("--------------------------")
-                count = 1
-                for key, value in reversed(tempPath.items()):
-                    reversedDict[key] = "BusID - " + str(value)
-                    print(f"{count:<3}| {key:<12}({reversedDict[key]})")
-                    count += 1
-
-                # Prints path found and duration of journey
-                print("\nJourney time in minutes:", totalTime)
-                return tempPath
-
             # If current node not in both unvisited and visited set, add it to unvisited and note neighbour as its parent
-            if node not in unvisited and node not in visited:
-                unvisited.add(node)
-                previous[node] = neighbour
-                time[node] = time[neighbour] + weight
+            if node not in visited:
+                newTime = time[neighbour] + weight
 
-            # Otherwise, check if quicker to visit other neighbours first, then node
-            # If yes, update parent data and time data and if the node was in the visited, move it to unvisited
-            else:
-                if time[node] > time[neighbour] + weight:
-                    time[node] = time[neighbour] + weight
+                if node not in time or newTime < time[node]:
+                    time[node] = newTime
+                    f = newTime + getHeuristic(node, endNode) 
                     previous[node] = neighbour
-
-                    if node in visited:
-                        visited.remove(node)
-                        unvisited.add(node)
-
-        # Remove neighbour from unvisited and add to visited because edges already inspected
-        unvisited.remove(neighbour)
-        visited.add(neighbour)
+                    unvisited.put((f, node))
 
     print("No path found")
     return None
@@ -172,6 +128,35 @@ def getCoordinatesOfBusStop(busstopID):
 
     return result
 
+# Method to generate and display graph with weight and edges
+def VisualiseGraph(multi_graph):
+    # Create a layout for the nodes
+    pos = nx.spring_layout(multi_graph)
+
+    # Draw nodes and edges with labels
+    nx.draw(multi_graph, pos, with_labels=True)
+
+    # Get a dictionary of edge labels
+    edge_labels = {}
+    for (u, v, key, data) in multi_graph.edges(keys=True, data=True):
+        route = data['route']
+        weight = data['weight']
+        label = (route, weight)
+        if (u, v) in edge_labels:
+            edge_labels[(u, v)].append(label)
+        else:
+            edge_labels[(u, v)] = [label]
+
+    # Draw edge labels
+    for edge, labels in edge_labels.items():
+        label = '\n'.join([f"{label[0]}: {label[1]}" for label in labels])
+        nx.draw_networkx_edge_labels(multi_graph, pos, edge_labels={(edge[0], edge[1]): label})
+
+
+    # Display the visualization and save to file
+    # plt.savefig("amogus.jpg", format='jpg')
+    plt.show()
+
 # Method to pre-load graph
 def load_pickle (filepath):
     # Create file object accessing the pickle file
@@ -191,8 +176,8 @@ graph = load_pickle('flask_application/setup/graph.pkl')
 # print(vars(graph))
 # VisualiseGraph(graph)
 
-currentBusStop = 1 #'A'
-endBusStop = 40 #'F'
+currentBusStop = 69
+endBusStop = 141
 
 aStarAlgo(currentBusStop, endBusStop)
 
