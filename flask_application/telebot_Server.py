ae088d69e701517f4f08805732cbfa07658c5855
skyish242 <atap2749@gmail.com>
2023-04-02 14:37:53 +0800

    tele changes

--- flask_application/telebot_Server.py
+++ flask_application/telebot_Server.py
@@ -17,9 +17,6 @@ ERROR_HEADER = "ERROR: "
 def sample_responses(input_text):
     user_message = str(input_text).lower()
 
-    if user_message in ('hello', 'hi'):
-        return "Hi!"
-
     return "Press /start lah..."
 
 def handle_message(update, context):
